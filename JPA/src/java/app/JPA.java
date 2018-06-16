e4be4968c4721fc03ac98bc835cbb400cd3db7dd
fabri <fabricio.somini@gmail.com>
2018-06-16 15:54:15 -0300

    Adicionado Eclipse e JPA

---
+++ JPA/src/java/app/JPA.java
@@ -0,0 +1,18 @@
+package app;
+
+import javax.persistence.*;
+
+public class JPA {
+    
+    public static EntityManagerFactory emf;
+    
+    public static EntityManager getEM() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(
+                    "JPAPU");
+        }
+        
+        return emf.createEntityManager();
+    }
+    
+}
