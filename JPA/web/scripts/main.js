01ef44c877de99200ededef6bbcfdf6b75901053
fabri <fabricio.somini@gmail.com>
2018-06-23 22:13:14 -0300

    Removido estilo do PrimeFaces

---
+++ JPA/web/scripts/main.js
@@ -0,0 +1,15 @@
+(() => {
+    $(function () {
+        $("[data-toggle=popover]").popover({
+            html: true,
+            content: function () {
+                return $('.popover-body').html();
+            },
+            title: function () {
+                var title = $(this).attr("data-popover-content");
+                return $(title).children(".popover-heading").html();
+            },
+            animation: true
+        });
+    });
+})();
